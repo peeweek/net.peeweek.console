d0d4f3126ae3bb2f2a006fe34d2a076dad688ca1
Thomas ICHÉ <peeweek@gmail.com>
2021-06-13 09:03:09 +0200

    Added Recorders

--- Console/Views/PerformanceView.cs
+++ Console/Views/PerformanceView.cs
@@ -2,48 +2,101 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.Profiling;
 using Unity.Profiling;
+using System.Collections.Generic;
 
 namespace ConsoleUtility
 {
     [RegisterStatView("fps")]
     public class PerformanceView : View
     {
-        ProfilerRecorder mainThreadTimeRecorder;
-        ProfilerRecorder renderThreadTimeRecorder;
-
-
         public PerformanceView() : base(20f) { }
+
+        Profiler mainThread;
+        Profiler renderThread;
+        Profiler gpu;
+        Profiler drawCall;
+        Profiler triangles;
+        Profiler shadowCasters;
+
 
         public override void OnEnable()
         {
             base.OnEnable();
-            mainThreadTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "Main Thread", 1);
-            renderThreadTimeRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Render Thread", 1);
+            mainThread = new Profiler(ProfilerCategory.Internal, "Main Thread");
+            renderThread = new Profiler(ProfilerCategory.Internal, "Gfx.WaitForRenderThread");
+            gpu = new Profiler(ProfilerCategory.Internal, "Gfx.PresentFrame");
+            drawCall = new Profiler(ProfilerCategory.Render, "Draw Calls Count");
+            triangles = new Profiler(ProfilerCategory.Render, "Triangles Count");
+            shadowCasters = new Profiler(ProfilerCategory.Render, "Shadow Casters Count");
         }
 
         public override void OnDisable()
         {
             base.OnDisable();
-            mainThreadTimeRecorder.Dispose();
-            renderThreadTimeRecorder.Dispose();
+
+
         }
 
         public override string GetDebugViewString()
         {
-            float ms = Time.smoothDeltaTime;
-            float main =    (float) (mainThreadTimeRecorder.LastValueAsDouble * (1e-6)); 
-            float render =  (float) (renderThreadTimeRecorder.LastValueAsDouble * (1e-6));
+            float ms = Time.deltaTime;
 
             return $@"
     Performance Statistics
     ======================
 
-    Framerate:              {(1f / ms).ToString("F2")}FPS
-        » Overall :         {(ms * 1000).ToString("F2")} ms
-        » Main Thread :     {(main).ToString("F2")}ms
-        » Render Thread :   {(render).ToString("F2")} ms
+    Framerate:                    {(1f / ms).ToString("F2")}FPS
+        » Overall       :         {(ms * 1000).ToString("F2")} ms
+        » Main Thread   :         {(mainThread.GetMilliseconds()).ToString("F2")} ms
+        » Render Thread :         {(renderThread.GetMilliseconds()).ToString("F2")} ms
+        » GPU           :         {(gpu.GetMilliseconds()).ToString("F2")} ms
+    Other:
+        » DrawCalls     :         {(drawCall.GetValue())} calls
+        » Triangles     :         {(triangles.GetValue())} triangles
+        » ShadowCasters :         {(shadowCasters.GetValue())} calls
+
 
 ";
+        }
+
+        class Profiler
+        {
+            ProfilerRecorder m_Recorder;
+
+            public Profiler(ProfilerCategory category, string name, int samples = 5)
+            {
+                m_Recorder = ProfilerRecorder.StartNew(category, name, samples);
+                m_Samples = new List<ProfilerRecorderSample>(samples);
+            }
+
+            ~Profiler()
+            {
+                m_Recorder.Dispose();
+            }
+
+            List<ProfilerRecorderSample> m_Samples;
+
+            public float GetMilliseconds()
+            {
+                return GetValue() * 0.000001f;
+            }
+
+            public long GetValue()
+            {
+                int samplesCount = m_Recorder.Capacity;
+                if (samplesCount == 0)
+                    return 0;
+
+                m_Recorder.CopyTo(m_Samples);
+
+                long r = 0;
+                for (var i = 0; i < m_Samples.Count; ++i)
+                    r += m_Samples[i].Value;
+
+                r /= m_Samples.Count;
+
+                return r;
+            }
         }
     }
 }
